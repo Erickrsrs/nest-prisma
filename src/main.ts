6b4d675d05918ac40e5a6bcf404d52bd62b934c5
Erick Rocha <erick2003rocha@gmail.com>
2023-02-08 09:21:03 -0300

    create errors interceptors

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NotFoundInterceptor } from './common/errors/interceptors/notfound.interceptor';
+import { UnauthorizedInterceptor } from './common/errors/interceptors/unauthorized.interceptor';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
@@ -13,6 +15,10 @@ async function bootstrap() {
     }),
   );
   app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalInterceptors(
+    new UnauthorizedInterceptor(),
+    new NotFoundInterceptor(),
+  );
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
