cde33e7e5ccce62cb04889a8921867a5e82dffa6
Erick Rocha <erick2003rocha@gmail.com>
2023-02-09 15:13:48 -0300

    add swagger documentation

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,14 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class CreatePostDto {
+  @ApiProperty({ description: 'Posts title' })
   @IsString()
   @IsNotEmpty()
   title: string;
 
+  @ApiProperty({ description: 'Posts content' })
   @IsString()
   @IsOptional()
   content?: string;
 
+  @ApiProperty({ description: 'Users email' })
   @IsEmail()
   authorEmail: string;
 }
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,14 +1,21 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsBoolean, IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateUserDto {
+  @ApiProperty({ description: 'User Email' })
   @IsEmail()
   @IsNotEmpty()
   email: string;
 
+  @ApiProperty({ description: 'Users full name' })
   @IsString()
   @IsNotEmpty()
   name: string;
 
+  @ApiProperty({
+    description: 'Defines whether the user is an admin',
+    default: false,
+  })
   @IsBoolean()
   admin: boolean;
 }
